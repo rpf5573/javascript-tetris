cf36832d9696d0222e57410f4f461c121ad9a6a1
comseung18 <comseung18@koreatech.ac.kr>
2020-11-21 17:28:32 +0900

    이미지 경로 처리

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const join = require('path').join;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const join = path.join;
+const resolve = path.resolve;
 
 module.exports = {
   entry: join(__dirname, '/src/main.ts'),
@@ -13,15 +16,10 @@ module.exports = {
     rules: [
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [{ 
-          loader: 'file-loader',
-          options: {
-            context: join(__dirname, "src/"),
-            outputPath: '/assets',
-            // publicPath: '/dist',
-            // useRelativePaths: true
-          }
-        }]
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[contenthash].[ext]'
+        }
       },
       {
         test: /\.(ts|tsx)$/,
@@ -30,7 +28,14 @@ module.exports = {
       },
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          "css-loader", "sass-loader"]
       },
     ]
   },
@@ -40,7 +45,10 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: join(__dirname, '/src/index.html')
-    })
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
   ],
   resolve: {
     extensions: ['.js', '.ts', '.json', 'scss']
